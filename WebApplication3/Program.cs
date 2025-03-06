a92d6e38c30ded6022214ed7191bbdc8d1fc39f6
MarkEmad656 <61595150+mark701@users.noreply.github.com>
2025-03-06 05:28:26 +0200

    commit

--- WebApplication3/Program.cs
+++ WebApplication3/Program.cs
@@ -105,6 +105,14 @@ if (app.Environment.IsDevelopment())
 
 app.MapControllers();
 
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+app.Urls.Add($"http://0.0.0.0:{port}");
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    db.Database.Migrate();
+}
+
 app.Run();
 
 
